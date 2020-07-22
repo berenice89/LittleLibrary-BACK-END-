b92cb3a7e4b7ab53857ed73181249f90003197b8
berenice89 <berenice.despres@gmail.com>
2020-07-22 16:10:32 +0200

    added POST and PUT routes

--- routes/books.js
+++ routes/books.js
@@ -38,4 +38,35 @@ router.get("/:id", (req, res) => {
   );
 });
 
+router.post("/", (req, res) => {
+  req.body.rate = parseFloat(req.body.rate);
+  console.log(req.body);
+  db.query("INSERT INTO book SET ?", [req.body], (err, results) => {
+    if (err) {
+      res.status(500).send("Erreur 500, pb SQL");
+      console.log(err.sql);
+      console.log(err.message);
+      return;
+    }
+    res.send(results);
+  });
+});
+
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+  db.query("UPDATE book SET ? WHERE id=?", [req.body, id], (err, results) => {
+    if (err) {
+      res.status(500).send("Erreur 500, pb SQL");
+      console.log(err.sql);
+      console.log(err.message);
+      return;
+    }
+    if (!results) {
+      res.status(400).send("Résultat vide");
+      return;
+    }
+    res.send(results);
+  });
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
 app.use("/books", require("./routes/books"));
 
 app.listen(3000, () => {
